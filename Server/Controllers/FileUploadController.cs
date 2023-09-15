c449943ea765d3acc55c9c46106e47e1e05fabdb
Peter Gallagher <pete@pjgcreations.co.uk>
2023-09-15 01:15:39 +0100

    Better paragraph extraction

--- Server/Controllers/FileUploadController.cs
+++ Server/Controllers/FileUploadController.cs
@@ -16,7 +16,6 @@ namespace YourProjectName.Controllers
         [HttpPost]
         public async Task<IActionResult> Upload(IFormFile file)
         {
-
             try
             {
                 if (file == null || file.Length == 0)
@@ -29,45 +28,79 @@ namespace YourProjectName.Controllers
                     await file.CopyToAsync(stream);
                     stream.Position = 0;
 
+                    // ...  
                     using (PdfDocument document = PdfDocument.Open(stream))
                     {
                         List<ExtractedParagraph> extractedParagraphs = new List<ExtractedParagraph>();
+                        string pendingContent = ""; // Add this variable to store content that needs to be appended  
 
                         for (int pageIndex = 0; pageIndex < document.NumberOfPages; pageIndex++)
                         {
-
                             Page page = document.GetPage(pageIndex + 1);
 
+                            // Extract words and blocks    
                             var words = NearestNeighbourWordExtractor.Instance.GetWords(page.Letters);
                             var blocks = DocstrumBoundingBoxes.Instance.GetBlocks(words);
 
-                            string text = page.Text;
-
-                            // Extract paragraphs from the text  
-                            var paragraphs = text.Split(new[] { "\r\n", "\r", "\n" }, StringSplitOptions.RemoveEmptyEntries);
-
-                            for (int paragraphIndex = 0; paragraphIndex < paragraphs.Length; paragraphIndex++)
+                            // Combine words in each block into paragraphs    
+                            int paragraphIndex = 0;
+                            foreach (var block in blocks)
                             {
-                                extractedParagraphs.Add(new ExtractedParagraph
+                                var paragraphContent = string.Join(" ", block.Text);
+
+                                // Check if the pending content should be appended  
+                                if (!string.IsNullOrEmpty(pendingContent))
                                 {
-                                    Id = $"{pageIndex + 1}.{paragraphIndex + 1}",
-                                    Title = $"Page {pageIndex + 1} - Paragraph {paragraphIndex + 1}",
-                                    Content = paragraphs[paragraphIndex]
-                                });
+                                    paragraphContent = pendingContent + " " + paragraphContent;
+                                    pendingContent = "";
+                                }
+
+                                // Count the number of words in the paragraph  
+                                int wordCount = paragraphContent.Split(' ').Length;
+
+                                // If the paragraph has less than 50 words, store it in pendingContent  
+                                if (wordCount < 50)
+                                {
+                                    pendingContent = paragraphContent;
+                                }
+                                else
+                                {
+                                    // Add the paragraph to the extractedParagraphs list  
+                                    extractedParagraphs.Add(new ExtractedParagraph
+                                    {
+                                        Id = $"{pageIndex + 1}.{paragraphIndex + 1}",
+                                        Title = $"Page {pageIndex + 1} - Paragraph {paragraphIndex + 1}",
+                                        Content = paragraphContent
+                                    });
+                                    paragraphIndex++;
+                                }
                             }
+                        }
+
+                        // Add any remaining pending content as the last paragraph  
+                        if (!string.IsNullOrEmpty(pendingContent))
+                        {
+                            extractedParagraphs.Add(new ExtractedParagraph
+                            {
+                                Id = $"{document.NumberOfPages}.{extractedParagraphs.Count + 1}",
+                                Title = $"Page {document.NumberOfPages} - Paragraph {extractedParagraphs.Count + 1}",
+                                Content = pendingContent
+                            });
                         }
 
                         return Ok(extractedParagraphs);
                     }
+
                 }
             }
             catch (Exception ex)
             {
-
-                throw;
+                // Log the exception or handle it as needed  
+                return StatusCode(StatusCodes.Status500InternalServerError, "An error occurred while processing the file.");
             }
-            
         }
+
+
 
         public class ExtractedParagraph
         {
