1a06d15d51d832e770739dbe85d132ea93a1dda6
Peter Gallagher <pete@pjgcreations.co.uk>
2024-04-13 09:31:15 +0100

    Added and updated Maui Project.

---
+++ maui/MainPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace maui
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        
+    }
+
+}
