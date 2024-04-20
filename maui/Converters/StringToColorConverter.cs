e77f0f42f63b3c51b2ea0b0d1917527f144ef32e
Clifford Agius <CliffordAgius@ca-software.co.uk>
2024-04-20 13:17:11 +0100

    Added some converters to change the Type of AI/Human into a bool and color for use in the MainPage.xamlso that the layout can be changed based on the type of message.  Still needs some cleanup but it works as needed...

---
+++ maui/Converters/StringToColorConverter.cs
@@ -0,0 +1,25 @@
+﻿using System.Globalization;
+
+namespace maui.Converters
+{
+    public class StringToColorConverter :IValueConverter
+    {
+        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            string type = value as string ?? string.Empty;
+            if (type is "ai" || type is "AI")
+            {
+                return Color.FromArgb("#550082cf");
+            }
+            else
+            {
+                return Color.FromArgb("#558ae28a");
+            }
+        }
+
+        public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
